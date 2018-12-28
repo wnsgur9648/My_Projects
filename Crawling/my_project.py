3ba08c84cb9ab91714dd226b00492d5e0c528f01
wnsgur9648 <wnsgur9648@naver.com>
2018-12-28 20:31:46 +0900

    181228

--- Crawling/my_project.py
+++ Crawling/my_project.py
@@ -1,14 +1,19 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = "https://www.youtube.com/channel/UC4ab7V7p1hW4YiuCRZL1Bzg"
+url = "https://www.youtube.com/channel/UC4ab7V7p1hW4YiuCRZL1Bzg/videos"
 
 response = requests.get(url).text
 document = bs(response, "html.parser")
-div = document.select("div h3")
-            
-for i in div:
-    print(i.text)
+a = document.select("h3 > a")
+
+for i in a:
+    url = "https://www.youtube.com/" + i["href"]
+    response = requests.get(url).text
+    document = bs(response, "html.parser")
+    a = document.select("div")
+
+    print(a)
             
 
     
